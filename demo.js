4261ff93cdadc4a8d4ce5a3425a354dc684a6f7c
VIKASH251204 <vikashtailor2512@gmail.com>
2024-11-27 13:55:56 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,15 +1,1 @@
-for(let i=1; i<= 5; i++ ){
-	let str = "";
-	let num = "";
-	
-	for(let j=1; j<=i; j++){
-		num=  num+j;
-	} document.write(num);
-	for(let d=i-1; d>=1; d--){
-		str = d + str;
-		
-		
-	} document.write(str,"<br>");
-
-}
 
